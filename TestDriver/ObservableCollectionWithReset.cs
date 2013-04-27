c5dff15597afa2deb2bd1c13eba47a9b52b64652
Dijji_cp <SND\Dijji_cp>
2013-04-27 10:58:50 +0000

    Fix copyright notices

--- TestDriver/ObservableCollectionWithReset.cs
+++ TestDriver/ObservableCollectionWithReset.cs
@@ -1,5 +1,4 @@
-﻿// Copyright (c) 2013, Dijii, and released under the Common Public License.  This, with other relevant licenses, can be found in the root of this distribution.
-
+﻿// Copyright (c) 2013, Dijji, and released under Ms-PL.  This, with other relevant licenses, can be found in the root of this distribution.
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
