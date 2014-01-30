aaeef591472b49f7e3a85ea925304c1353126fd4
Dijji_cp <SND\Dijji_cp>
2014-01-30 07:55:40 +0000

    Update version information for File Meta 1.2 RC

--- AssociationManager/Properties/AssemblyInfo.cs
+++ AssociationManager/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Dijji")]
 [assembly: AssemblyProduct("File Metadata")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright ©  2013, 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,4 +51,4 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyVersion("1.2.*")]
