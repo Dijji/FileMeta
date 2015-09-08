12f8ede72bac49bc89166c471424cd131cba9c8e
Dijji_cp <SND\Dijji_cp>
2015-09-08 08:03:59 +0000

    Add Gong Solutions WPF drag-and-drop support to the project

---
+++ GongSolutions.Wpf.DragDrop/DropTargetAdorner.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Documents;
+using System.Windows;
+using System.Windows.Media;
+
+namespace GongSolutions.Wpf.DragDrop
+{
+  public abstract class DropTargetAdorner : Adorner
+  {
+    public DropTargetAdorner(UIElement adornedElement)
+      : base(adornedElement)
+    {
+      this.m_AdornerLayer = AdornerLayer.GetAdornerLayer(adornedElement);
+      this.m_AdornerLayer.Add(this);
+      this.IsHitTestVisible = false;
+    }
+
+    public void Detatch()
+    {
+      this.m_AdornerLayer.Remove(this);
+    }
+
+    public DropInfo DropInfo { get; set; }
+
+    internal static DropTargetAdorner Create(Type type, UIElement adornedElement)
+    {
+      if (!typeof(DropTargetAdorner).IsAssignableFrom(type)) {
+        throw new InvalidOperationException(
+          "The requested adorner class does not derive from DropTargetAdorner.");
+      }
+
+      return (DropTargetAdorner)type.GetConstructor(new[] { typeof(UIElement) })
+                                    .Invoke(new[] { adornedElement });
+    }
+
+    private readonly AdornerLayer m_AdornerLayer;
+  }
+}
---
+++ GongSolutions.Wpf.DragDrop/Utilities/TypeUtilities.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Reflection;
+
+namespace GongSolutions.Wpf.DragDrop.Utilities
+{
+  public class TypeUtilities
+  {
+    public static IEnumerable CreateDynamicallyTypedList(IEnumerable source)
+    {
+      var type = GetCommonBaseClass(source);
+      var listType = typeof(List<>).MakeGenericType(type);
+      var addMethod = listType.GetMethod("Add");
+      var list = listType.GetConstructor(Type.EmptyTypes).Invoke(null);
+
+      foreach (var o in source) {
+        addMethod.Invoke(list, new[] { o });
+      }
+
+      return (IEnumerable)list;
+    }
+
+    public static Type GetCommonBaseClass(IEnumerable e)
+    {
+      var types = e.Cast<object>().Select(o => o.GetType()).ToArray<Type>();
+      return GetCommonBaseClass(types);
+    }
+
+    public static Type GetCommonBaseClass(Type[] types)
+    {
+      if (types.Length == 0) {
+        return typeof(object);
+      }
+
+      var ret = types[0];
+
+      for (var i = 1; i < types.Length; ++i) {
+        if (types[i].IsAssignableFrom(ret)) {
+          ret = types[i];
+        } else {
+          // This will always terminate when ret == typeof(object)
+          while (!ret.IsAssignableFrom(types[i])) {
+            ret = ret.BaseType;
+          }
+        }
+      }
+
+      return ret;
+    }
+  }
+}
