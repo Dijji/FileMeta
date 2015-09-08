bc86d526f1d05e27f19bcb907bcc78e5259a79e0
Dijji_cp <27084934+Dijji@users.noreply.github.com>
2015-09-08 08:03:59 +0000

    Add Gong Solutions WPF drag-and-drop support to the project

---
+++ GongSolutions.Wpf.DragDrop/DefaultDragHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Windows;
+using GongSolutions.Wpf.DragDrop.Utilities;
+
+namespace GongSolutions.Wpf.DragDrop
+{
+  public class DefaultDragHandler : IDragSource
+  {
+    public virtual void StartDrag(IDragInfo dragInfo)
+    {
+      var itemCount = dragInfo.SourceItems.Cast<object>().Count();
+
+      if (itemCount == 1) {
+        dragInfo.Data = dragInfo.SourceItems.Cast<object>().First();
+      } else if (itemCount > 1) {
+        dragInfo.Data = TypeUtilities.CreateDynamicallyTypedList(dragInfo.SourceItems);
+      }
+
+      dragInfo.Effects = (dragInfo.Data != null) ?
+                           DragDropEffects.Copy | DragDropEffects.Move :
+                           DragDropEffects.None;
+    }
+
+    public bool CanStartDrag(IDragInfo dragInfo)
+    {
+      return true;
+    }
+
+    public virtual void Dropped(IDropInfo dropInfo)
+    {
+    }
+
+    public virtual void DragCancelled()
+    {
+    }
+  }
+}
---
+++ GongSolutions.Wpf.DragDrop/Utilities/VisualTreeExtensions.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Collections.Generic;
+using System.Windows.Media.Media3D;
+
+namespace GongSolutions.Wpf.DragDrop.Utilities
+{
+  public static class VisualTreeExtensions
+  {
+    internal static DependencyObject FindVisualTreeRoot(this DependencyObject d)
+    {
+      var current = d;
+      var result = d;
+
+      while (current != null) {
+        result = current;
+        if (current is Visual || current is Visual3D) {
+          break;
+        } else {
+          // If we're in Logical Land then we must walk 
+          // up the logical tree until we find a 
+          // Visual/Visual3D to get us back to Visual Land.
+          current = LogicalTreeHelper.GetParent(current);
+        }
+      }
+
+      return result;
+    }
+
+    public static T GetVisualAncestor<T>(this DependencyObject d) where T : class
+    {
+      var item = VisualTreeHelper.GetParent(d.FindVisualTreeRoot());
+
+      while (item != null) {
+        var itemAsT = item as T;
+        if (itemAsT != null) {
+          return itemAsT;
+        }
+        item = VisualTreeHelper.GetParent(item);
+      }
+
+      return null;
+    }
+
+    public static DependencyObject GetVisualAncestor(this DependencyObject d, Type type)
+    {
+      var item = VisualTreeHelper.GetParent(d.FindVisualTreeRoot());
+
+      while (item != null && type != null) {
+        if (item.GetType() == type || item.GetType().IsSubclassOf(type)) {
+          return item;
+        }
+        item = VisualTreeHelper.GetParent(item);
+      }
+
+      return null;
+    }
+
+    /// <summary>
+    /// find the visual ancestor by type and go through the visual tree until the given itemsControl will be found
+    /// </summary>
+    public static DependencyObject GetVisualAncestor(this DependencyObject d, Type type, ItemsControl itemsControl)
+    {
+      var item = VisualTreeHelper.GetParent(d.FindVisualTreeRoot());
+      DependencyObject lastFoundItemByType = null;
+
+      while (item != null && type != null) {
+        if (item == itemsControl) {
+          return lastFoundItemByType;
+        }
+        if (item.GetType() == type || item.GetType().IsSubclassOf(type)) {
+          lastFoundItemByType = item;
+        }
+        item = VisualTreeHelper.GetParent(item);
+      }
+
+      return lastFoundItemByType;
+    }
+
+    public static T GetVisualDescendent<T>(this DependencyObject d) where T : DependencyObject
+    {
+      return d.GetVisualDescendents<T>().FirstOrDefault();
+    }
+
+    public static IEnumerable<T> GetVisualDescendents<T>(this DependencyObject d) where T : DependencyObject
+    {
+      var childCount = VisualTreeHelper.GetChildrenCount(d);
+
+      for (var n = 0; n < childCount; n++) {
+        var child = VisualTreeHelper.GetChild(d, n);
+
+        if (child is T) {
+          yield return (T)child;
+        }
+
+        foreach (var match in GetVisualDescendents<T>(child)) {
+          yield return match;
+        }
+      }
+
+      yield break;
+    }
+  }
+}
