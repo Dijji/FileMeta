093138ddf7cc165aa7fb21e3e6370994c996ce93
Dijji_cp <27084934+Dijji@users.noreply.github.com>
2016-09-03 11:51:46 +0000

    Support chaining to existing handlers

--- PropertyHandler/PropertyHandler.cpp
+++ PropertyHandler/PropertyHandler.cpp
@@ -13,7 +13,7 @@ static const WCHAR* PropertyHandlerDescription = L"File Metadata Property Handle
 class CPropertyHandler : public IPropertyStore, public IInitializeWithFile
 {
 public:
-    CPropertyHandler() : _cRef(1), _pStore(NULL), _bReadWrite(FALSE)
+    CPropertyHandler() : _cRef(1), _pStore(NULL), _bReadWrite(FALSE), _pChainedPropStore(NULL), _bHaveChainedPropCount(FALSE)
     {
         DllAddRef();
     }
@@ -60,15 +60,20 @@ private:
 	~CPropertyHandler()
     {
         SafeRelease(&_pStore);
+		SafeRelease(&_pChainedPropStore);
         DllRelease();
     }
 	HRESULT OpenStore(BOOL bReadWrite);
+	DWORD ChainedPropCount();
 
     long _cRef;
 
 	WCHAR					_pszFilePath[MAX_PATH];
 	BOOL		            _bReadWrite;     // Whether storage set currently open read write
-	IPropertyStore *		_pStore;		// Wrapper over set of storages
+	IPropertyStore *		_pStore;		// Wrapper over set of storages.
+    IPropertyStore *		_pChainedPropStore;	// Chained properties store
+	BOOL					_bHaveChainedPropCount; // Whether we have read the count of chained properties
+    DWORD					_cChainedPropCount;	// Count of properties in the chained properties store
 };
 
 HRESULT CPropertyHandler_CreateInstance(REFIID riid, void **ppv)
@@ -87,24 +92,41 @@ HRESULT CPropertyHandler::GetCount(DWORD *pcProps)
 {
     *pcProps = 0;
 	HRESULT hr = OpenStore(FALSE);
-    return SUCCEEDED(hr) ? _pStore->GetCount(pcProps) : hr;
+    hr = SUCCEEDED(hr) ? _pStore->GetCount(pcProps) : hr;
+    if (SUCCEEDED(hr))
+         *pcProps += ChainedPropCount();
+    return hr;
 }
 
 HRESULT CPropertyHandler::GetAt(DWORD iProp, PROPERTYKEY *pkey)
 {
     *pkey = PKEY_Null;
 	HRESULT hr = OpenStore(FALSE);
-    return SUCCEEDED(hr) ? _pStore->GetAt(iProp, pkey) : hr;
+	// We take chained properties first because their number remains constant,
+	// whereas the number of File Meta properties varies as they are set,
+	// which would mean that if we took File Meta properties first,
+	// repeated calls with the same index into the chained properties could return different key values
+    hr = SUCCEEDED(hr) ?
+        (iProp < ChainedPropCount()) ?
+		    _pChainedPropStore->GetAt(iProp, pkey) :
+            _pStore->GetAt(iProp - ChainedPropCount(), pkey) :
+        hr;
+    return hr;
 }
 
 HRESULT CPropertyHandler::GetValue(REFPROPERTYKEY key, PROPVARIANT *pPropVar)
 {
     PropVariantInit(pPropVar);
 	HRESULT hr = OpenStore(FALSE);
-    return SUCCEEDED(hr) ? _pStore->GetValue(key, pPropVar) : hr;
+	// Take the File Meta property value first, and if there isn't one,
+	// try for a chained property value
+    hr = SUCCEEDED(hr) ? _pStore->GetValue(key, pPropVar) : hr;
+    if (_pChainedPropStore != NULL && SUCCEEDED(hr) && pPropVar->vt == VT_EMPTY)
+        hr = _pChainedPropStore->GetValue(key, pPropVar);
+    return hr;
 }
 
-// SetValue just updates the property store's value cache
+// SetValue just updates the File Meta property store's value cache
 HRESULT CPropertyHandler::SetValue(REFPROPERTYKEY key, REFPROPVARIANT propVar)
 {
 	HRESULT hr = OpenStore(TRUE);
@@ -155,6 +177,20 @@ HRESULT CPropertyHandler::OpenStore(BOOL bReadWrite)
 	return hr;
 }
 
+DWORD CPropertyHandler::ChainedPropCount()
+{
+	if (!_bHaveChainedPropCount)
+	{
+		_cChainedPropCount = 0;
+
+		if (_pChainedPropStore != NULL)
+			_bHaveChainedPropCount = SUCCEEDED(_pChainedPropStore->GetCount(&_cChainedPropCount));
+		else
+			_bHaveChainedPropCount = TRUE;
+	}
+	return _cChainedPropCount;
+}
+
 HRESULT CPropertyHandler::Initialize(LPCWSTR pszFilePath, DWORD grfMode)
 {
     HRESULT hr = S_OK;
@@ -166,6 +202,66 @@ HRESULT CPropertyHandler::Initialize(LPCWSTR pszFilePath, DWORD grfMode)
 		BOOL fRunningOnNT = ((GetVersion() & 0x80000000) != 0x80000000);
 		v_pfnStgOpenStorageEx = ((fRunningOnNT) ? (PFN_STGOPENSTGEX)GetProcAddress(GetModuleHandle(L"OLE32"), "StgOpenStorageEx") : NULL);
 	}
+
+    // Check if a chained property handler is configured, and if there is one, load and initialize it too.
+	// This is simplified by the fact that we only ever open a chained property handler read-only
+    SafeRelease(&_pChainedPropStore);
+    PCWSTR pszExt = PathFindExtension(_pszFilePath);
+    if (*pszExt)
+    {
+        // Chained property handlers are configured in a Chained property value added to the standard property system key
+        HKEY propertyHandlerKey;
+		WCHAR szKeyName[MAX_PATH] = L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\PropertySystem\\PropertyHandlers\\";
+		wcscat_s(szKeyName, MAX_PATH, pszExt);
+        if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, szKeyName, 0, KEY_QUERY_VALUE, &propertyHandlerKey) == ERROR_SUCCESS)
+        {
+            WCHAR szBuf[64];
+            DWORD nBufLen = sizeof(szBuf);
+            DWORD dwType;
+
+            if (RegGetValue(propertyHandlerKey, NULL, L"Chained", RRF_RT_REG_SZ, &dwType, szBuf, &nBufLen) == ERROR_SUCCESS)
+            {
+                CLSID clsid;
+                if (SUCCEEDED(CLSIDFromString(szBuf, &clsid)))
+                {
+                    HRESULT hr2 = CoCreateInstance(clsid, NULL, CLSCTX_ALL, IID_IPropertyStore, (void **)&_pChainedPropStore);
+                    if (SUCCEEDED(hr2))
+                    {
+                        IInitializeWithFile *pChainedPropInit;
+                        hr2 = _pChainedPropStore->QueryInterface(IID_IInitializeWithFile, (void **)&pChainedPropInit);
+                        if (SUCCEEDED(hr2))
+                        {
+                            hr2 = pChainedPropInit->Initialize(_pszFilePath, STGM_READ);
+                            pChainedPropInit->Release();
+                        }
+                        else
+                        {
+                            IInitializeWithStream *pChainedPropInitWithStream;
+                            hr2 = _pChainedPropStore->QueryInterface(IID_IInitializeWithStream, (void **)&pChainedPropInitWithStream);
+                            if (SUCCEEDED(hr2))
+                            {
+                                IStream *pStream;
+                                hr2 = SHCreateStreamOnFileEx(_pszFilePath, STGM_READ, 0, FALSE, NULL, &pStream);
+                                if (SUCCEEDED(hr2))
+                                {
+                                    hr2 = pChainedPropInitWithStream->Initialize(pStream, STGM_READ);
+                                    pStream->Release();
+                                }
+                                pChainedPropInitWithStream->Release();
+                            }
+                        }
+                        if (FAILED(hr2))
+                        {
+                            _pChainedPropStore->Release();
+                            _pChainedPropStore = NULL;
+                        }
+                    }
+                }
+            }
+
+            RegCloseKey(propertyHandlerKey);
+        }
+ 	}
 
     return hr;
 }
