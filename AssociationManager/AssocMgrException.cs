b13674e78083e0f584882d61a0b2ec6e23773042
Dijji_cp <SND\Dijji_cp>
2016-10-11 13:11:29 +0000

    Support centralisation of message resources, command line association management, 32-bit handlers on 64-bit systems, and update of previous generations of registry entry

---
+++ AssociationManager/AssocMgrException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FileMetadataAssociationManager
+{
+    class AssocMgrException : Exception
+    {
+        public string Description { get; set; }
+        public Exception Exception { get; set; }
+        public WindowsErrorCodes ErrorCode { get; set; }
+        public string DisplayString
+        {
+            get
+            {
+                if (Exception == null)
+                    return Description;
+                else
+                    return Description + "\r\n" + Exception.Message;
+            }
+        }
+    }
+}
+;
