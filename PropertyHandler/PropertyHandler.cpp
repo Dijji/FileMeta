c5124f92a613d9d422e5f258858f8471247b8696
Dijji <27084934+Dijji@users.noreply.github.com>
2020-05-02 16:10:09 +0100

    Working property handler logging

--- PropertyHandler/PropertyHandler.cpp
+++ PropertyHandler/PropertyHandler.cpp
@@ -3,16 +3,107 @@
 #include <windows.h>
 #include <shlwapi.h>
 #include <propkey.h>
-#include <strsafe.h>
 #include <propvarutil.h>
 #include "dll.h"
 #include "RegisterExtension.h"
+
+#pragma region Logging support functions
+
+#include <strsafe.h>
 #include <iostream>
 #include <string>
 #include <fstream>
 #include <cstdarg>
 #include <vector>
 #include <ctime>
+
+std::wstring FormatGuid(GUID guid)
+{
+	WCHAR buffer[250];
+	swprintf_s(buffer, 250, L"{%08lX-%04hX-%04hX-%02hhX%02hhX-%02hhX%02hhX%02hhX%02hhX%02hhX%02hhX}",
+		guid.Data1, guid.Data2, guid.Data3,
+		guid.Data4[0], guid.Data4[1], guid.Data4[2], guid.Data4[3],
+		guid.Data4[4], guid.Data4[5], guid.Data4[6], guid.Data4[7]);
+	std::wstring result(buffer);
+	return result;
+}
+
+std::wstring FormatVariant(REFPROPVARIANT propvar)
+{
+	WCHAR szBuffer[128];
+
+	HRESULT hr = PropVariantToString(propvar, szBuffer, ARRAYSIZE(szBuffer));
+
+	if (SUCCEEDED(hr) || hr == STRSAFE_E_INSUFFICIENT_BUFFER)
+		return std::wstring(szBuffer);
+	else
+		return std::wstring(L"???");
+}
+
+std::wstring getCurrentDateTime(std::wstring s)
+{
+	time_t now = std::time(0);
+	struct tm  tstruct;
+	WCHAR  buf[80];
+	localtime_s(&tstruct, &now);
+	if (s == L"now")
+		wcsftime(buf, sizeof(buf), L"%Y-%m-%d %X", &tstruct);
+	else if (s == L"date")
+		wcsftime(buf, sizeof(buf), L"%Y-%m-%d", &tstruct);
+	return std::wstring(buf);
+}
+
+BOOL DirectoryExists(std::wstring dirName) {
+	DWORD attribs = ::GetFileAttributes(dirName.c_str());
+	if (attribs == INVALID_FILE_ATTRIBUTES) {
+		return false;
+	}
+	return (attribs & FILE_ATTRIBUTE_DIRECTORY);
+}
+
+std::wstring PropName(REFPROPERTYKEY key)
+{
+	IPropertyDescription *pPropDesc;
+	std::wstring result(L"Unknown");
+	HRESULT hr = PSGetPropertyDescription(key, IID_IPropertyDescription, (void**)&pPropDesc);
+	if (SUCCEEDED(hr))
+	{
+		LPWSTR pszName;
+		hr = pPropDesc->GetCanonicalName(&pszName);
+		if (SUCCEEDED(hr))
+		{
+			result = std::wstring(pszName);
+			CoTaskMemFree(pszName);
+		}
+		SafeRelease(&pPropDesc);
+	}
+	return result;
+}
+
+void WriteLog(const WCHAR* fmt, ...)
+{
+	// Format the message
+	WCHAR buffer[512];
+	va_list vl;
+	va_start(vl, fmt);
+	_vsnwprintf_s(buffer, ARRAYSIZE(buffer), _TRUNCATE, fmt, vl);
+	va_end(vl);
+
+	// Write the message to the log
+	std::wstring path = L"c:\\FilemetaLogs";
+	if (!DirectoryExists(path) &&
+		!CreateDirectory(path.c_str(), NULL))
+		return; // Maybe throw an exception?
+	std::wstring filePath = path + L"\\log_" + getCurrentDateTime(L"date") + L".txt";
+	std::wofstream ofs(filePath.c_str(), std::ios_base::out | std::ios_base::app);
+	std::wstring now = getCurrentDateTime(L"now");
+	ofs << now << L"  " << buffer << L'\n';
+	ofs.flush();
+	ofs.close();
+}
+
+
+#pragma endregion // logging functions 
 
 extern PFN_STGOPENSTGEX v_pfnStgOpenStorageEx;
 
@@ -32,7 +123,7 @@ public:
         static const QITAB qit[] =
         {
             QITABENT(CPropertyHandler, IPropertyStore),
-            QITABENT(CPropertyHandler, IInitializeWithFile),
+			QITABENT(CPropertyHandler, IInitializeWithFile),
             {0, 0 },
         };
         return QISearch(this, qit, riid, ppv);
@@ -48,6 +139,7 @@ public:
         long cRef = InterlockedDecrement(&_cRef);
         if (cRef == 0)
         {
+			WriteLog(L"Final release received for instance");
             delete this;
         }
         return cRef;
@@ -71,7 +163,7 @@ private:
 		SafeRelease(&_pChainedPropStore);
         DllRelease();
     }
-	HRESULT OpenStore(BOOL bReadWrite);
+	HRESULT OpenStore(BOOL bRead);
 	DWORD ChainedPropCount();
 
     long _cRef;
@@ -96,103 +188,22 @@ HRESULT CPropertyHandler_CreateInstance(REFIID riid, void **ppv)
     return hr;
 }
 
-void format(std::string& a_string, const char* fmt, ...)
-{
-	va_list vl;
-	va_start(vl, fmt);
-	int size = _vscprintf(fmt, vl);
-	if (size > 0)
-	{
-		a_string.resize(++size);
-		vsnprintf_s((char*)a_string.data(), size, _TRUNCATE, fmt, vl);
-	}
-	va_end(vl);
-}
-
-std::string FormatGuid(GUID guid)
-{
-	std::string result;
-	format (result, "{%08lX-%04hX-%04hX-%02hhX%02hhX-%02hhX%02hhX%02hhX%02hhX%02hhX%02hhX}",
-		guid.Data1, guid.Data2, guid.Data3,
-		guid.Data4[0], guid.Data4[1], guid.Data4[2], guid.Data4[3],
-		guid.Data4[4], guid.Data4[5], guid.Data4[6], guid.Data4[7]);
-	return result;
-}
-
-std::wstring FormatVariant(REFPROPVARIANT propvar)
-{
-	WCHAR szBuffer[128];
-
-	HRESULT hr = PropVariantToString(propvar, szBuffer, ARRAYSIZE(szBuffer));
-
-	if (SUCCEEDED(hr) || hr == STRSAFE_E_INSUFFICIENT_BUFFER)
-		return szBuffer;
-	else
-		return L"???";
-}
-
-std::string getCurrentDateTime(std::string s)
-{
-	time_t now = std::time(0);
-	struct tm  tstruct;
-	char  buf[80];
-	localtime_s(&tstruct, &now);
-	if (s == "now")
-		strftime(buf, sizeof(buf), "%Y-%m-%d %X", &tstruct);
-	else if (s == "date")
-		strftime(buf, sizeof(buf), "%Y-%m-%d", &tstruct);
-	return std::string(buf);
-}
-
-BOOL DirectoryExists(std::string dirName) {
-	DWORD attribs = ::GetFileAttributesA(dirName.c_str());
-	if (attribs == INVALID_FILE_ATTRIBUTES) {
-		return false;
-	}
-	return (attribs & FILE_ATTRIBUTE_DIRECTORY);
-}
-
-
-void WriteLogMsg(const std::string& message)
-{
-	// Write the message to the log
-	std::string path = "c:\\Filemeta logs";
-	if (!DirectoryExists(path) &&
-		!CreateDirectoryA(path.c_str(), NULL))
-			return; // Maybe throw an exception?
-	std::string filePath = path + "\\log_" + getCurrentDateTime("date") + ".txt";
-	std::string now = getCurrentDateTime("now");
-	std::ofstream ofs(filePath.c_str(), std::ios_base::out | std::ios_base::app);
-	ofs << now << '\t' << message << '\n';
-	ofs.close();
-}
-void WriteLog(const char* fmt, ...)
-{
-	// Format the message
-	std::string message;
-	va_list vl;
-	va_start(vl, fmt);
-	format(message, fmt, vl);
-	va_end(vl);
-	WriteLogMsg(message);
-}
-
 HRESULT CPropertyHandler::GetCount(DWORD *pcProps)
 {
 	try
 	{
-		WriteLogMsg("GetCount called");
+		WriteLog(L"GetCount called");
 		*pcProps = 0;
 		HRESULT hr = OpenStore(FALSE);
 		hr = SUCCEEDED(hr) ? _pStore->GetCount(pcProps) : hr;
 		if (SUCCEEDED(hr))
 			*pcProps += ChainedPropCount();
-		WriteLog("GetCount returning 0x%X with value %u", hr, *pcProps);
+		WriteLog(L"GetCount returned 0x%08X with value %u", hr, *pcProps);
 		return hr;
 	}
 	catch (...)
 	{
-		WriteLogMsg("Unknown exception in GetCount");
+		WriteLog(L"Unknown exception in GetCount");
 		return E_FAIL;
 	}
 }
@@ -201,7 +212,7 @@ HRESULT CPropertyHandler::GetAt(DWORD iProp, PROPERTYKEY *pkey)
 {
 	try
 	{
-		WriteLog("GetAt called for index %d", iProp);
+		WriteLog(L"GetAt called for index %d", iProp);
 		*pkey = PKEY_Null;
 		HRESULT hr = OpenStore(FALSE);
 		// We take chained properties first because their number remains constant,
@@ -213,12 +224,13 @@ HRESULT CPropertyHandler::GetAt(DWORD iProp, PROPERTYKEY *pkey)
 			_pChainedPropStore->GetAt(iProp, pkey) :
 			_pStore->GetAt(iProp - ChainedPropCount(), pkey) :
 			hr;
-		WriteLog("GetAt returning 0x%X with value %s %d", hr, FormatGuid(pkey->fmtid), pkey->pid);
+		WriteLog(L"GetAt returned 0x%08X with value %s = %s %d", hr, PropName(*pkey).c_str(),
+			FormatGuid(pkey->fmtid).c_str(), pkey->pid);
 		return hr;
 	}
 	catch (...)
 	{
-		WriteLogMsg("Unknown exception in GetAt");
+		WriteLog(L"Unknown exception in GetAt");
 		return E_FAIL;
 	}
 }
@@ -227,7 +239,7 @@ HRESULT CPropertyHandler::GetValue(REFPROPERTYKEY key, PROPVARIANT *pPropVar)
 {
 	try
 	{
-		WriteLog("GetValue called for %s %d", FormatGuid(key.fmtid), key.pid);
+		WriteLog(L"GetValue called for %s = %s %d", PropName(key).c_str(), FormatGuid(key.fmtid).c_str(), key.pid);
 		PropVariantInit(pPropVar);
 		HRESULT hr = OpenStore(FALSE);
 		// Take the File Meta property value first, and if there isn't one,
@@ -238,15 +250,15 @@ HRESULT CPropertyHandler::GetValue(REFPROPERTYKEY key, PROPVARIANT *pPropVar)
 			hr = InitPropVariantFromString(L"FileMetadata", pPropVar);
 		else if (_pChainedPropStore != NULL && SUCCEEDED(hr) && pPropVar->vt == VT_EMPTY)
 		{
-			WriteLogMsg("Value will be taken from chained property store");
+			WriteLog(L"Value will be taken from chained property store");
 			hr = _pChainedPropStore->GetValue(key, pPropVar);
 		}
-		WriteLog("GetValue returning 0x%X with value %S", hr, FormatVariant(*pPropVar));
+		WriteLog(L"GetValue returned 0x%08X with value '%s'", hr, FormatVariant(*pPropVar).c_str());
 		return hr;
 	}
 	catch (...)
 	{
-		WriteLogMsg("Unknown exception in GetValue");
+		WriteLog(L"Unknown exception in GetValue");
 		return E_FAIL;
 	}
 }
@@ -256,14 +268,17 @@ HRESULT CPropertyHandler::SetValue(REFPROPERTYKEY key, REFPROPVARIANT propVar)
 {
 	try
 	{
-		WriteLog("SetValue called for %s %d with value %S", FormatGuid(key.fmtid), key.pid, FormatVariant(propVar));
+		WriteLog(L"SetValue called with value '%s' for %s = %s %d", FormatVariant(propVar).c_str(),
+			PropName(key).c_str(), FormatGuid(key.fmtid).c_str(), key.pid);
 		HRESULT hr = OpenStore(TRUE);
-		return SUCCEEDED(hr) ? _pStore->SetValue(key, propVar) : hr;
-		WriteLog("SetValue returning 0x%X", hr);
+		if (SUCCEEDED(hr))
+			hr = _pStore->SetValue(key, propVar);
+		WriteLog(L"SetValue returned 0x%08X", hr);
+		return hr;
 	}
 	catch (...)
 	{
-		WriteLogMsg("Unknown exception in SetValue");
+		WriteLog(L"Unknown exception in SetValue");
 		return E_FAIL;
 	}
 }
@@ -273,15 +288,15 @@ HRESULT CPropertyHandler::Commit()
 {
 	try
 	{
-		WriteLogMsg("Commit called");
+		WriteLog(L"Commit called");
 		HRESULT hr = OpenStore(TRUE);
 		if (SUCCEEDED(hr))
 			hr = _pStore->Commit();
-		WriteLog("Commit returning 0x%X", hr);
+		WriteLog(L"Commit returned 0x%08X", hr);
 	}
 	catch (...)
 	{
-		WriteLogMsg("Unknown exception in Commit");
+		WriteLog(L"Unknown exception in Commit");
 		return E_FAIL;
 	}
 }
@@ -305,7 +320,7 @@ HRESULT CPropertyHandler::OpenStore(BOOL bReadWrite)
 		IPropertySetStorage* pPropSetStg = NULL;
 		DWORD dwReadWrite = bReadWrite ? STGM_READWRITE : STGM_READ;
 
-		WriteLog("Changing property store access to %s", bReadWrite ? "Read/Write" : "Read");
+		WriteLog(L"Opening property store %s", bReadWrite ? L"Read/Write" : L"Read only");
 		hr = (v_pfnStgOpenStorageEx)(_pszFilePath, dwReadWrite | STGM_SHARE_EXCLUSIVE, STGFMT_FILE, 0, NULL, 0, 
 				IID_IPropertySetStorage, (void**)&pPropSetStg);
 
@@ -317,7 +332,7 @@ HRESULT CPropertyHandler::OpenStore(BOOL bReadWrite)
 
 		SafeRelease(&pPropSetStg);
 
-		WriteLog("Property store access change returned 0x%X", hr);
+		WriteLog(L"Open property store returned 0x%08X", hr);
 		if (SUCCEEDED(hr))
 			_bReadWrite = bReadWrite;
 	}
@@ -333,9 +348,9 @@ DWORD CPropertyHandler::ChainedPropCount()
 
 		if (_pChainedPropStore != NULL)
 		{
-			WriteLogMsg("Calling chained GetCount");
+			WriteLog(L"Calling chained GetCount");
 			_bHaveChainedPropCount = SUCCEEDED(_pChainedPropStore->GetCount(&_cChainedPropCount));
-			WriteLog("Chains GetCount returned %u", _cChainedPropCount);
+			WriteLog(L"Chains GetCount returned %u", _cChainedPropCount);
 		}
 		else
 			_bHaveChainedPropCount = TRUE;
@@ -348,7 +363,9 @@ HRESULT CPropertyHandler::Initialize(LPCWSTR pszFilePath, DWORD grfMode)
 	try
 	{
 		HRESULT hr = S_OK;
-		WriteLog("Initialize for '%S' mode=s", pszFilePath);// , grfMode == 0 ? "Read" : "Read/Write");
+		WCHAR buffer[250];
+		swprintf_s(buffer, 250, L"Initialize for '%s' mode=%s", pszFilePath, (grfMode == 0 ? L"Read" : L"Read/Write"));
+		WriteLog(L"Initialize for '%s' mode=%s", pszFilePath, (grfMode == 0 ? L"Read" : L"Read/Write"));
 
 		wcscpy_s(_pszFilePath, MAX_PATH, pszFilePath);
 
@@ -379,7 +396,7 @@ HRESULT CPropertyHandler::Initialize(LPCWSTR pszFilePath, DWORD grfMode)
 					CLSID clsid;
 					if (SUCCEEDED(CLSIDFromString(szBuf, &clsid)))
 					{
-						WriteLog("Attempting to initialize read-only chained property handler: %s", FormatGuid(clsid));
+						WriteLog(L"Attempting to initialize read-only chained property handler: %s", FormatGuid(clsid).c_str());
 						HRESULT hr2 = CoCreateInstance(clsid, NULL, CLSCTX_ALL, IID_IPropertyStore, (void **)&_pChainedPropStore);
 						if (SUCCEEDED(hr2))
 						{
@@ -412,7 +429,7 @@ HRESULT CPropertyHandler::Initialize(LPCWSTR pszFilePath, DWORD grfMode)
 								_pChainedPropStore = NULL;
 							}
 						}
-						WriteLog("Chained property store Initialize returned 0x%X", hr);
+						WriteLog(L"Chained property store Initialize returned 0x%08X", hr);
 					}
 				}
 
@@ -424,7 +441,7 @@ HRESULT CPropertyHandler::Initialize(LPCWSTR pszFilePath, DWORD grfMode)
 	}
 	catch (...)
 	{
-		WriteLogMsg("Unknown exception in Initialize");
+		WriteLog(L"Unknown exception in Initialize");
 		return E_FAIL;
 	}
 }
